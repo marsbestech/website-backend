98090135c73537264f803c9c993f588ee32eed5d
mebi-wz <mehbubadilla@gmail.com>
2026-05-27 18:10:36 +0300

    api changed

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   user: process.env.DB_USER,
-  host: process.env.DB_HOST,
+  host:'127.0.0.1',
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
