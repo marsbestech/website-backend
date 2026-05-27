751c6a5a45345c20dbe8f3b93ebf82b5e3ea8574
mebi-wz <mehbubadilla@gmail.com>
2026-05-27 18:55:01 +0300

    disable database ssl by default

--- db.js
+++ db.js
@@ -6,9 +6,7 @@ const pool = new Pool({
     database: process.env.DB_NAME,
     password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT || 5432,
-    ssl: {
-        rejectUnauthorized: false  // ← allows self-signed certs on cPanel
-    }
+    ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false
 });
 
 pool.connect(async (err, client, release) => {
