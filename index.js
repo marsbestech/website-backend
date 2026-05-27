82ca2767e9e26c626112c3285b05b4f545427bcf
mebi-wz <mehbubadilla@gmail.com>
2026-05-27 18:02:02 +0300

    api changed

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const apiRoutes = require('./routes/index');
 
 const app = express();
 
-// Lock CORS to your React domain
 app.use(cors({
     origin: process.env.CLIENT_URL || 'https://marsbestech.com',
     credentials: true
@@ -14,8 +13,6 @@ app.use(cors({
 
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-
-// Mount the modular routes at /api
 app.use('/api', apiRoutes);
 
 module.exports = app;
