830080496d451df5d9827f727521cac943f4adde
mebi-wz <mehbubadilla@gmail.com>
2026-05-30 18:33:17 +0300

    version changes5

--- app.js
+++ app.js
@@ -11,6 +11,12 @@ app.use(cors({
     credentials: true
 }));
 
+// Request logger middleware
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+});
+
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
@@ -19,8 +25,8 @@ app.get('/', (req, res) => {
     res.json({ status: 'ok', message: 'Marsbes Tech API is running' });
 });
 app.get('/version-test', (req, res) => {
+    console.log('Version test endpoint hit');
     res.json({ status: 'ok', message: 'version updated' });
-
 });
 // API routes
 app.use('/api', apiRoutes);
